89b4f8c8c8843ecd708b1ed77dfa34b8ef588c53
jnixon7007 <jnixon7007@sohamcollege.org.uk>
2014-05-08 11:52:05 +0100

    Update greenBottles.py
    added a comment

--- greenBottles.py
+++ greenBottles.py
@@ -1,3 +1,4 @@
+#this is my green bottle code########
 bottles=10
 for x in range(bottles,0,-1):
     print("{0} green bottle hanging on the wall.\n{0} green bottles hanging on the wall.".format(x))
